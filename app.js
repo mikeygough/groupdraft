e882f0bf00e1cfa14e46df22813dc75867816d5b
mikey <mikey@hey.com>
2024-07-07 21:35:41 -0500

    Complete test email send

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+// env
+require('dotenv').config();
+
 // express
 const express = require('express');
 const app = express();
@@ -17,6 +20,46 @@ const { engine } = require('express-handlebars');
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.use('/public', express.static('public'));
+
+// mailgun
+const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
+
+// mailgun auth
+const auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  },
+};
+
+// create mailer
+const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+
+// SEND EMAIL
+const user = {
+  email: 'mikey@hey.com',
+  name: 'mikey',
+  age: '18',
+};
+
+nodemailerMailgun
+  .sendMail({
+    from: 'no-reply@example.com',
+    to: user.email, // An array if you have multiple recipients.
+    subject: 'Hey you, awesome!',
+    template: {
+      name: 'email.handlebars',
+      engine: 'handlebars',
+      context: user,
+    },
+  })
+  .then((info) => {
+    console.log('Response: ' + info);
+  })
+  .catch((err) => {
+    console.log('Error: ' + err);
+  });
 
 // app
 app.get('/', (req, res) => {
